d196f719f81e943e61a9590287d3695d970b5d9c
Jonghyeon <flowithusdev@gmail.com>
2021-09-23 00:40:30 +0900

    setting: Webpack 설정: style/css/sass-loader, html-loader, file-loader, html-webpack-plugin, clean-webpack-plugin, webpack-merge

---
+++ youtube/webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        // 순서가 뒤에서 부터 시작함
+        use: [
+          'style-loader', // 3. styles를 DOM에 삽입
+          'css-loader', // 2. css를 commonjs로 변환
+          'sass-loader', // 1. scss를 css로 변환
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+          },
+        },
+      },
+    ],
+  },
+};
