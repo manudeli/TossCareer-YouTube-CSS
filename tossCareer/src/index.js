b62377cf6823f654572daac4e12765eb93691296
Jonghyeon Ko <flowithusdev@gmail.com>
2021-09-26 06:49:02 +0900

    feat: parallax test

--- tossCareer/src/index.js
+++ tossCareer/src/index.js
@@ -2,14 +2,14 @@ import './styles/main.scss';
 import { querySelector } from './utils/domUtil';
 import { css, html } from 'lit';
 
-window.addEventListener('scroll', (e) => {
+window.addEventListener('scroll', () => {
   const target = querySelector('.scroll');
   const scrolled = window.pageYOffset;
   const rate = scrolled * 1;
 
   console.log(rate);
   target.style = css`
-    transform: translate(0, ${rate / 2}px);
+    transform: translate(0, ${rate * 2}px);
     opacity: ${rate > 100 ? 0 : 1};
     transition: opacity 1s;
   `;
